1ea94fbfc3780236bc0a10df8e50fbd20d6165ec
SoulDevt <souleyboyz@gmail.com>
2022-12-16 13:09:49 +0100

    first commit

---
+++ src/components/Field.vue
@@ -0,0 +1,21 @@
+<script setup>
+    defineProps({
+        type: {
+            type: String,
+            required: true,
+            default: "email"
+        },
+        name: {
+            type: String,
+            required: true,
+            default: "email"
+        }
+    })
+</script>
+
+
+<template>
+    <h2>uwu</h2>
+</template>
+
+<style scoped></style>
