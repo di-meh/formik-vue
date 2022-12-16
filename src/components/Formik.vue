8862d570a7ea183c947c06bf6c3b4a5c32205b90
Mehdi Saber <m1d3s@protonmail.com>
2022-12-16 14:30:13 +0100

    Inject into Field

--- src/components/Formik.vue
+++ src/components/Formik.vue
@@ -3,6 +3,26 @@
 </template>
 
 <script setup>
+import {provide, reactive} from "vue";
+
+const props = defineProps({
+  initialValues: {
+    type: Object,
+    required: true,
+    default: {}
+  },
+  onSubmit: {
+    type: Function,
+    required: true
+  },
+  validate: {
+    type: Function,
+    required: false,
+    default: null
+  }
+});
+const values = reactive(props.initialValues);
+provide('initialValues', values);
 
 </script>
 
