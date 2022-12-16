538c5aef94cc056624121aedd7ab6393d0a8a7ff
Mehdi Saber <m1d3s@protonmail.com>
2022-12-16 22:49:08 +0100

    Fix errors not cleaning

--- src/components/Formik.vue
+++ src/components/Formik.vue
@@ -16,23 +16,24 @@ const props = defineProps({
 });
 
 const isSubmitting = ref(false);
-const errors = reactive([]);
+const errors = ref([]);
 const values = ref({...props.initialValues});
 
 const setSubmitting = (value) => isSubmitting.value = value;
 const resetValues = () => values.value = props.initialValues;
 const handleSubmit = () => {
   isSubmitting.value = true;
+  errors.value = [];
   if (props.validate) {
-    Object.assign(errors, props.validate(values.value));
+    Object.assign(errors.value, props.validate(values.value));
   }
 
-  if (Object.keys(errors).length === 0) {
+  if (Object.keys(errors.value).length === 0) {
     props.onSubmit(values, {setSubmitting, resetValues});
   }
   else {
     isSubmitting.value = false;
-    errors.forEach((error) => console.error(error));
+    errors.value.forEach((error) => console.error(error));
   }
   console.log(toRaw(values.value), "handleSubmit");
 }
